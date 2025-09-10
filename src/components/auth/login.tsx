66f805e6705598e1379b3ca8055babb65d2753bb
LAPTOP-0RUBD428\XLOC <tathankochi@gmail.com>
2025-09-10 22:48:48 +0700

    feat(auth): integrate NextAuth for authentication and add login functionality
    - Added NextAuth configuration with credentials provider for user authentication.
    - Implemented login logic in the Login component using the authenticate function.
    - Created custom error classes for handling authentication errors.
    - Updated package dependencies to include next-auth and query-string.
    - Introduced new types for user and authentication responses.

--- src/components/auth/login.tsx
+++ src/components/auth/login.tsx
@@ -2,11 +2,14 @@
 import { Button, Col, Divider, Form, Input, Row } from 'antd';
 import { ArrowLeftOutlined } from '@ant-design/icons';
 import Link from 'next/link';
+import { authenticate } from '@/utils/actions';
 
 const Login = () => {
 
     const onFinish = async (values: any) => {
-
+        const { email, password } = values;
+        const res = await authenticate(email, password);
+        console.log(">>> check res: ", res)
     };
 
     return (
